f46df17f0206f51077c9b5a5c06a9ab5843bde7f
Darren Wong <darrenwong23@gmail.com>
2015-05-16 20:11:21 -0700

    convert to qwerty

--- qwertyPiano.js
+++ qwertyPiano.js
@@ -3,13 +3,14 @@ var synth = new AudioSynth;
 var piano = Synth.createInstrument('piano');
 
 var keyMappings = {
-	a: 'C',
-	s: 'D',
-	d: 'E',
-	f: "F", 
-	g: "G", 
-	h: "A", 
-	j: "B"
+	"a": 'C',
+	"s": 'D',
+	"d": 'E',
+	"f": "F", 
+	"g": "G", 
+	"h": "A", 
+	"j": "B",
+	"_": "_"
 };
 
 $(document).keypress(function(e){
@@ -17,17 +18,38 @@ $(document).keypress(function(e){
 	piano.play(key , 4, 3);
 })
 
-var twinkleBeginning = [a, a, g, g, h, h, g];
+Object.prototype.getKeyByValue = function( value ) {
+    for( var prop in this ) {
+        if( this.hasOwnProperty( prop ) ) {
+             if( this[ prop ] === value )
+                 return prop;
+        }
+    }
+}
+// var twinkleBeginning = [a, a, g, g, h, h, g];
+var twinkle = "ccgg_aa_gffeeddcggffeedccgg_aa_gffeeddc";
+var convertToQWERTY = function(string) {
+	var qwertyKeys = string;
+
+	for(var i = 0; i<string.length; i++) {
+		string[i] = keyMappings.getKeyByValue(string[i].toUpperCase());
+	}
+	return qwertyKeys; 
+}
+console.log(convertToQWERTY(twinkle));
+
 
 var startPlaying = function() {
-	var $letters = $(".score").children();
-	console.log($letters);
+	var letter = $(".notes").children()[0];
+		$(letter).toggleClass("current");
 
-	for(var i = 0; i< $letters.length; i++) {
+	setInterval(function(){
+		$(letter).toggleClass("current");
+		letter = $(letter).next();
+		$(letter).toggleClass("current");
 
-	};
-
-}
+	}, 500)
+};
 
 
 
